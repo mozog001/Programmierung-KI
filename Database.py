8eb1dc7d53e7eeddefbccfd9f400dcf3c6e461d3
Andreas Happ <happ.andreas@fh-swf.de>
2023-11-26 01:09:37 +0100

    Init TestDatabase.py

--- Database.py
+++ Database.py
@@ -1,21 +1,50 @@
 import sqlite3
 
 
+
+
 class StockDatabase:
 
     """ Database for stock data
+        Um die Datenbank zu testen, kann die Datei TestDatabase.py ausgeführt werden.
+        Die Datenbank wird dann mit Testdaten gefüllt.
+        Die Datenbank wird in der Datei StockDatabase.db gespeichert.
+        Die Datenbank enthält 3 Tabellen:
+
+        1. links: Enthält die Links zu den einzelnen Aktien
+        2. stock_data: Enthält die historischen Daten der Aktien
+        3. symbols: Enthält die Symbole der Aktien
+
+        Die Datenbank wird mit der Klasse StockDatabase erstellt.
+        Die Klasse enthält folgende Methoden:
+
+        1. insert_link: Fügt einen Link in die Tabelle links ein
+
+        In Pycharm kann die Datenbank mit dem Plugin "DB Browser for SQLite" betrachtet werden.
     """
 
-    def __init__(self, db):
-        self.conn = sqlite3.connect(db)
+    def __init__(self):
+        # Stellt eine Verbindung zur lokalen Datenbank "StockDatabase.db her
+        self.conn = sqlite3.connect('StockDatabase.db')
         self.cur = self.conn.cursor()
+        # Erstelle notwendige Tabellen "Datenmodellierung" --> links, stock_data, symbols
         self.cur.execute(
-            "CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, isbn INTEGER)")
+            "CREATE TABLE IF NOT EXISTS links (id INTEGER PRIMARY KEY, date TEXT, link TEXT, stock_name TEXT UNIQUE)")
+
+        self.cur.execute(
+            "CREATE TABLE IF NOT EXISTS stock_data (id INTEGER PRIMARY KEY, date TEXT, open REAL, high REAL, \
+            low REAL,close REAL, volume INT, stock_long_name TEXT UNIQUE, stock_currency TEXT, stock_industry TEXT ,\
+            stock_headquarter TEXT)")
+
+        self.cur.execute(
+            "CREATE TABLE IF NOT EXISTS symbols (id INTEGER PRIMARY KEY, symbol TEXT UNIQUE, name TEXT UNIQUE, \
+            country TEXT,ipo_year INTEGER, sector TEXT, industry TEXT)")
+
         self.conn.commit()
 
-    def insert(self, title, author, year, isbn):
+    def insert_link(self, date, link, stock_name):
         self.cur.execute(
-            "INSERT INTO book VALUES (NULL, ?, ?, ?, ?)", (title, author, year, isbn))
+            "INSERT INTO links VALUES (NULL, ?, ?, ?)", (date, link, stock_name))
         self.conn.commit()
 
     def view(self):
---
+++ TestDatabase.py
@@ -0,0 +1,11 @@
+import Database as db
+
+# Test Database
+db = db.StockDatabase()
+db.insert_link("2020-01-01", "https://www.test.com", "Test")
+db.insert_link("2020-01-02", "https://www.test.com", "Test2")
+db.insert_link("2020-01-03", "https://www.test.com", "Test3")
+
+
+
+
