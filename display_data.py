74b5eb025771428a2d3c2fe5b136f3549b7245f0
Omar Elgammal <dev.elgammal@gmail.com>
2023-12-04 21:28:33 +0100

    create a file that contains functions that map and output the data needed for GUI

---
+++ display_data.py
@@ -0,0 +1,28 @@
+import API as api
+
+# get a list of stock prices
+def get_stocks_list(stock_symbol, start_date, end_date):
+    # Fetch stock data
+    data = api.StockData(stock_symbol)
+    stock_data = data.get_stock_data(start_date, end_date)
+
+    # Turn data into a list of tuples [(date timestamp, open price, close price)]
+    data_list = []
+    for index_timestamp, row in stock_data.iterrows():
+        timestamp_to_local_str = index_timestamp.tz_convert('Europe/Berlin').strftime('%Y-%m-%d %H:%M:%S%z')
+        data_list.append((timestamp_to_local_str, row["Open"], row["Close"]))
+    return data_list
+
+print(get_stocks_list("AAPL", "2020-01-01", "2020-01-10"))
+
+#get a list of stock news links
+def get_stock_news(stock_symbol, date):
+    # Fetch stock News
+    data = api.StockData(stock_symbol)
+    stock_news = data.get_stock_news(date)
+
+    # Turn data into a list of links [link1, link2, ...]
+    links = stock_news["Link"].tolist()
+    return links
+
+print(get_stock_news("AAPL", "2020-01-01"))
