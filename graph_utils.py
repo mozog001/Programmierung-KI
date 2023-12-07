8e36c7cbd8c1ef999cbb5c9218064513c750b321
beefkasten <christoph.zaunig@mailbox.org>
2023-12-07 18:37:39 +0100

    Aufräumarbeit graph_utils

--- graph_utils.py
+++ graph_utils.py
@@ -1,28 +1,13 @@
 import random
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
+import display_data
 
-def generate_stock_prices(days, initial_price=100):
-    stock_prices = [initial_price]
+stock_data = display_data.get_stocks_list("AAPL", "2020-01-01", "2020-01-10")
 
-    for _ in range(1, days):
-        # Simuliere eine zufällige prozentuale Änderung des Aktienkurses
-        daily_change_percentage = random.uniform(-2, 2)  # Beispiel: Änderung zwischen -2% und 2%
-        
-        new_price = stock_prices[-1] * (1 + daily_change_percentage / 100)
-        stock_prices.append(new_price)
-
-    return stock_prices
-
-def genGraph(ax, canvas):
-    test_data = generate_stock_prices(350)
-    return test_data
-
-def calculate_sma(ax, canvas, test_data):
+def calculate_sma(stock_data):
     sma_values = []
-    window_size = 10
-    for i in range(len(test_data) - window_size + 1):
-        window = test_data[i : i + window_size]
+    window_size = 3
+    for i in range(len(stock_data) - window_size + 1):
+        window = stock_data[i : i + window_size]
         sma = sum(window) / window_size
         sma_values.append(sma)
     return sma_values
