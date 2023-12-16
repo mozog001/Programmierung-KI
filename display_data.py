9178ce2c96f6fc4868e61aafa987993c4d6e3468
beefkasten <christoph.zaunig@mailbox.org>
2023-12-16 18:04:10 +0100

    Add SMA Calculator

--- display_data.py
+++ display_data.py
@@ -13,7 +13,7 @@ def get_stocks_list(stock_symbol, start_date, end_date):
         data_list.append((timestamp_to_str, row["Open"], row["Close"]))
     return data_list
 
-print(get_stocks_list("AAPL", "2020-01-01", "2020-01-10"))
+#print(get_stocks_list("AAPL", "2020-01-01", "2020-01-10"))
 
 #get a list of stock news links
 def get_stock_news(stock_symbol, date):
@@ -25,4 +25,4 @@ def get_stock_news(stock_symbol, date):
     links = stock_news["Link"].tolist()
     return links
 
-print(get_stock_news("AAPL", "2020-01-01"))
+#print(get_stock_news("AAPL", "2020-01-01"))
--- graph_utils.py
+++ graph_utils.py
@@ -1,13 +1,15 @@
-import random
 import display_data
 
-stock_data = display_data.get_stocks_list("AAPL", "2020-01-01", "2020-01-10")
+test_data = display_data.get_stocks_list("AAPL", "2020-01-01", "2020-03-01")
 
-def calculate_sma(stock_data):
+def calculate_sma(stock_data, window_size):
     sma_values = []
-    window_size = 3
     for i in range(len(stock_data) - window_size + 1):
-        window = stock_data[i : i + window_size]
-        sma = sum(window) / window_size
-        sma_values.append(sma)
+        window = stock_data[i:i + window_size]        
+        # Liste mit Daten generieren
+        timestamp = window[-1][0]        
+        # Liste mit Preisen generieren
+        prices = [item[1] for item in window]        
+        sma = sum(prices) / len(prices)        
+        sma_values.append((timestamp, sma))    
     return sma_values
