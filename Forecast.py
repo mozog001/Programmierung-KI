3a8c8f4ebef1b5614432e940e322ba8557557111
Ilir <ajvazi.ilir@fh-swf.de>
2023-12-17 01:13:28 +0100

    Prognose

---
+++ Forecast.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from prophet import Prophet  # https://facebook.github.io/prophet/docs/quick_start.html
+from prophet.plot import plot_plotly, plot_components_plotly
+#  https://peerj.com/preprints/3190.pdf
+import Database as StockDB
+
+
+class Forecast:
+    def __init__(self, stockname):
+        self.stockname = stockname
+        self.stockDB = StockDB.StockDatabase()
+
+    def get_forecast(self):
+        data = self.stockDB.getStockCloseData(self.stockname)
+        data = pd.DataFrame(data, columns=['ds', 'y'])
+        forecast = Prophet()
+        forecast.fit(data)
+        future = forecast.make_future_dataframe(periods=365)
+        predict = forecast.predict(future)
+        return predict[["yhat", "yhat_lower", "yhat_upper"]]
+
+
+if __name__ == "__main__":
+    test = Forecast("TYEKF")
+    test_data = test.get_forecast()
+    print(test_data)
+
+
+# data = pd.read_csv("stock_data.csv")
+# data[['date', 'time']] = data['Date'].str.split(' ', expand=True)
+# df = data[['date', 'Close']]
+# df = df.rename(columns={"date": "ds", "Close": "y"})
+# m = Prophet()
+# m.fit(df)
+# future = m.make_future_dataframe(periods=365)
+# forecast = m.predict(future)
+# forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
+# plot_plotly(m, forecast).show()
+# plot_components_plotly(m, forecast).show()
+# print(forecast)
