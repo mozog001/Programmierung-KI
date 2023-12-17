046cbf2473fee8d73a8011592bd5ce7865e21b00
Ilir <ajvazi.ilir@fh-swf.de>
2023-12-17 17:07:40 +0100

    Symbols added

--- API.py
+++ API.py
@@ -21,11 +21,10 @@ class StockData:
         self.s_news = None
         self.s_data = None
         #  self.stockDB = StockDB.StockDatabase()
-        self.symbols = pd.read_csv("symbols.csv", sep=";")
-
-        self.symbols = self.symbols.rename(columns=str.lower)
-        self.symbols = self.symbols.to_dict('index')
-        stockDB.insert_symbol(self.symbols)
+        #  self.symbols = pd.read_csv("symbols.csv", sep=";")
+        #  self.symbols = self.symbols.rename(columns=str.lower)
+        #  self.symbols = self.symbols.to_dict('index')
+        #  stockDB.insert_symbol(self.symbols)
 
     def get_stock_data(self, start, end):
         self.start = start
